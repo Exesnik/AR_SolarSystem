5d1b828cff0ee997731f3b9793e5551f0301c914
Exesnik <92322311+Exesnik@users.noreply.github.com>
2023-09-21 15:58:42 +0400

    .

--- Assets/Scripts/SolarSystem/SpaceObject.cs
+++ Assets/Scripts/SolarSystem/SpaceObject.cs
@@ -8,7 +8,7 @@ public class SpaceObject : MonoBehaviour
     public byte id;
     public string nameObject;
     public  float speed;
-    public float scale;
+
 
     // public float distance; // пока не нужен
 
